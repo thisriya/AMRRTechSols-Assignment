a200c3068f155f50673e5d0fa568366850b92dc8
Riya Singh <singh.riyarajput2003@gmail.com>
2025-06-21 22:30:56 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,10 @@ function App() {
   return savedItems ? JSON.parse(savedItems) : [
     {
       id: 1,
-      name: 'Blue T-Shirt',
-      type: 'Shirt',
-      description: 'Comfortable cotton t-shirt in blue color',
-      coverImage: 'https://havencraft.co.ke/wp-content/uploads/2021/03/Royal-Blue-Front.jpg',
-      additionalImages: [
-        'https://via.placeholder.com/300x400?text=Front+View',
-        'https://via.placeholder.com/300x400?text=Back+View'
-      ]
-    },
-    {
-      id: 2,
-      name: 'Black Jeans',
+      name: 'Gray Jeans',
       type: 'Pant',
-      description: 'Slim fit black jeans',
-      coverImage: 'https://cdn-img.prettylittlething.com/e/6/8/7/e687ca660e5f0e8d44b7cb01e52a2ba916148406_cna8526_5.jpg',
+      description: 'Slim fit Gray jeans',
+      coverImage: 'https://www.bfgcdn.com/1500_1500_90/007-2147/arcteryx-gamma-ar-brushed-pant-winter-trousers-detail-2.jpg',
       additionalImages: [
         'https://via.placeholder.com/300x400?text=Front+View',
         'https://via.placeholder.com/300x400?text=Side+View'
